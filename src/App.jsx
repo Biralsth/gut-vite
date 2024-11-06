15a1948e7f0ef6e8c0cef72d8b52a970c9e18161
Biralsth <theonepieceisreal333@gmail.com>
2024-11-06 17:35:33 +0545

    First commit vite

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+// import './App.css'
+
+const App = () => {
+
+
+
+
+
+  return (
+    <>
+      <nav className=' py-5 px-10 flex items-center justify-between'>
+        <h2 className='text-2xl'>Gut</h2>
+        <div className='flex gap-8 item-center'>
+          <h3 className='text-xl'>About</h3>
+          <h3 className='text-xl'>Contact</h3>
+          <h3 className='text-xl'>Services</h3>
+          <h3 className='text-xl'>Account</h3>
+        </div>
+        {/* <div className="wow"></div> */}
+        {/* <div className="wow"></div> */}
+      </nav>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/Mouse.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Mouse = () => {
+  return (
+
+    <form>
+
+
+
+
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+
+  )
+}
+
+export default Mouse
